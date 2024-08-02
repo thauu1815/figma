b749f80ae977f929a8b1e2e39f100a51c95caf41
thauany <107870177+thauu1815@users.noreply.github.com>
2024-08-02 12:07:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,20 @@ const textos = document.querySelectorAll(".aba-conteudo");
 textos[i].classList.add("ativo");
 textos[j].classList.remove("ativo");
 const contadores = document.querySelectorAll(".contador");
-contadores[0]
-const tempoObjetivo1 = new Date("2024-10-05");
-contadores[0].textContent = tempoObjetivo1;
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+   return dias + "dias" + horas + "horas" + minutos + "minutos"  + segundos + "segundos";
+}
